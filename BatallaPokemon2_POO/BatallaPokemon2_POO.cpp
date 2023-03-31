f600f0f82605ce67c5fe8c2d1e8249c1f3d213f8
DanielKyjas <116916840+DanielKyjas@users.noreply.github.com>
2023-03-30 22:13:35 -0600

    Update BatallaPokemon2_POO.cpp

--- BatallaPokemon2_POO/BatallaPokemon2_POO.cpp
+++ BatallaPokemon2_POO/BatallaPokemon2_POO.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include<windows.h> 
 #include<string>
+#include <vector>
 using namespace std;
 
 class Pokemon
@@ -40,7 +41,7 @@ public:
     }
     ~Pokemon()
     {
-        cout << "El pokemon " << nombre << " ha sido derrotado\n";
+        
     }
 
     virtual void mostrar()
@@ -50,11 +51,22 @@ public:
         cout << "De color " << color << endl;
         cout << "Con un ataque de " << ataque << " de danio" << endl;
         cout << "Y tiene " << vida << " puntos de vida" << endl;
-        cout << "Seguro de tu eleccion? (1 para si y 0 para no)" << endl;
+    }
+    virtual void mostrarContricante()
+    {
+        cout << "Tu contricante invoco a " << nombre << endl;
+        cout << "Es tipo " << tipo << endl;
+        cout << "De color " << color << endl;
+        cout << "Con un ataque de " << ataque << " de danio" << endl;
+        cout << "Y tiene " << vida << " puntos de vida" << endl;
     }
     virtual void atacar()
     {
-        cout << "El pokemon ha atacado";
+        cout << "El pokemon ha atacado\n";
+    }
+    virtual void atacarfuerte()
+    {
+        cout << "El pokemon ha atacado fuerte\n";
     }
     string getnombre() { return nombre; }
     void setnombre(string name) { nombre = name; }
@@ -96,11 +108,17 @@ public:
     }
     ~PokemonAgua()
     {
-        cout << "El pokemon " << nombre << " ha sido derrotado\n";
+        
     }
     void atacar() override
     {
-        cout << "El pokemon ha usado torrente" << endl;
+        ataque = 15;
+        cout << nombre << " ha usado torrente" << endl;
+    }
+    void atacarfuerte() override
+    {
+        ataque = 22;
+        cout << nombre << " ha usado tsunami" << endl;
     }
 };
 class PokemonFuego : public Pokemon
@@ -128,11 +146,17 @@ public:
     }
     ~PokemonFuego()
     {
-        cout << "El pokemon " << nombre << " ha sido derrotado\n";
+       
     }
     void atacar() override
     {
-        cout << "El pokemon ha usado lanzallamas" << endl;
+        ataque = 25;
+        cout << nombre << " ha usado lanzallamas" << endl;
+    }
+    void atacarfuerte() override
+    {
+        ataque = 32;
+        cout << nombre << " ha usado bomba ignea" << endl;
     }
 };
 class PokemonPlanta : public Pokemon
@@ -160,11 +184,17 @@ public:
     }
     ~PokemonPlanta()
     {
-        cout << "El pokemon " << nombre << " ha sido derrotado\n";
+       
     }
     void atacar() override
     {
-        cout << "El pokemon ha usado hoja afilada" << endl;
+        ataque = 18;
+        cout << nombre << " ha usado hoja afilada" << endl;
+    }
+    void atacarfuerte() override
+    {
+        ataque = 25;
+        cout << nombre << " ha usado raiz mortifera" << endl;
     }
 };
 class PokemonElectrico : public Pokemon
@@ -192,19 +222,20 @@ public:
     }
     ~PokemonElectrico()
     {
-        cout << "El pokemon " << nombre << " ha sido derrotado\n";
+        
     }
     void atacar() override
     {
-        cout << "el pokemon ha usado trueno" << endl;
+        ataque = 22;
+        cout << nombre << " ha usado trueno" << endl;
+    }
+    void atacarfuerte() override
+    {
+        ataque = 28;
+        cout << nombre << " ha usado tormenta electrica" << endl;
     }
 };
 
-void AtaquePorTurnos(Pokemon* pokemon1, Pokemon* pokemon2)
-{
-        pokemon1->atacar();
-        pokemon2->setvida(pokemon2->getvida() - pokemon1->getataque());
-}
 
  void sanar(Pokemon* pokemon1)
 {
@@ -213,7 +244,7 @@ void AtaquePorTurnos(Pokemon* pokemon1, Pokemon* pokemon2)
     pokemon1->setvida(vida += 50);
     if (pokemon1->getvida() > pokemon1->getvida2())
     {
-        pokemon1->setvida(pokemon1->getvida());
+        pokemon1->setvida(pokemon1->getvida2());
     }
     cout << "El pokemon " << pokemon1->getnombre() << " se ha curado y ahora su vida es de " <<  pokemon1->getvida() << "\n";
 }
@@ -221,21 +252,46 @@ void AtaquePorTurnos(Pokemon* pokemon1, Pokemon* pokemon2)
  void destruir(Pokemon* pokemon1)
  {
      delete pokemon1;
+     cout << "El pokemon " << pokemon1->getnombre() << " ha sido derrotado" << endl;
+ }
+ void Ataquenormal(Pokemon* pokemon1, Pokemon* pokemon2)
+ {
+     pokemon1->atacar();
+     pokemon2->setvida(pokemon2->getvida() - pokemon1->getataque());
+     cout << "la vida de " << pokemon2->getnombre() << " es " << pokemon2->getvida() << endl;
+ }
+ void AtaqueFuerte(Pokemon* pokemon1, Pokemon* pokemon2, int random)
+ {
+     if (random <= 50) {
+         std::cout << pokemon1->getnombre() << " ha fallado\n";
+     }
+     else {
+         pokemon1->atacarfuerte();
+         pokemon2->setvida(pokemon2->getvida() - pokemon1->getataque());
+         std::cout << "El ataque fue efectivo y ahora " << pokemon2->getnombre() << " tiene " << pokemon2->getvida() << " puntos de vida\n";
+     }
  }
 
- void victoria(Pokemon* pokemon1)
+ void ganador(Pokemon* pokemon1)
  {
      cout << "El pokemon " << pokemon1->getnombre() << " ha resultado victorioso" << endl;
  }
 
+ void combatir(vector<Pokemon*> combatiente)
+ {
+     PokemonAgua* pokemonagua = (PokemonAgua*)combatiente[0];
+ }
 int main()
 {
     bool eleccion = false;
     int numeropokemon = 4;
-    int numeroeleccion = 4;
+    int numeroeleccion = 3;
     int victoria = 0;
+    int acertar = 0;
     srand(time(NULL));
-    Pokemon* combatiente = new Pokemon[2];
+
+    Pokemon* peleador = new Pokemon[2];
+
 
     PokemonAgua* greninja = new PokemonAgua();
     greninja->setnombre("greninja");
@@ -249,6 +305,12 @@ int main()
     PokemonElectrico* pikachu = new PokemonElectrico();
     pikachu->setnombre("pikachu");
 
+    /*PokemonElectrico* pichu = new PokemonElectrico("pichu", "electrico", "amarillo", 50, 60);
+    pichu->mostrar();*/
+
+    bulbasaur->atacar();
+    pikachu->atacar();
+    vector<Pokemon*> Combatiente;
     
     do {
         std::cout << "---------------------------------\n";
@@ -262,27 +324,48 @@ int main()
         switch (numeropokemon) {
         case 1:
             greninja->mostrar();
+            cout << "Seguro de tu eleccion? (1 para si y 0 para no)" << endl;
             cin >> eleccion;
+            if (eleccion != 1) {
+                break;
+                system("CLS");
+            }
             system("CLS");
-            combatiente[0] = *greninja;
+            peleador[0] = *greninja;
+            Combatiente.push_back(greninja);
             break;
         case 2:
             bulbasaur->mostrar();
+            cout << "Seguro de tu eleccion? (1 para si y 0 para no)" << endl;
             cin >> eleccion;
+            if (eleccion != 1) {
+                break;
+                system("CLS");
+            }
             system("CLS");
-            combatiente[0] = *bulbasaur;
+            Combatiente.push_back(bulbasaur);
             break;
         case 3:
             charizard->mostrar();
+            cout << "Seguro de tu eleccion? (1 para si y 0 para no)" << endl;
             cin >> eleccion;
+            if (eleccion != 1) {
+                break;
+                system("CLS");
+            }
             system("CLS");
-            combatiente[0] = *charizard;
+            Combatiente.push_back(charizard);
             break;
         case 4:
             pikachu->mostrar();
+            cout << "Seguro de tu eleccion? (1 para si y 0 para no)" << endl;
             cin >> eleccion;
+            if (eleccion != 1) {
+                break;
+                system("CLS");
+            }
             system("CLS");
-            combatiente[0] = *pikachu;
+            Combatiente.push_back(pikachu);
             break;
         default:
             eleccion = 0;
@@ -296,33 +379,56 @@ int main()
     }while(numeropokemon == tupokemon);
     switch (numeropokemon) {
     case 1:
-        combatiente[1] = *greninja;
+        peleador[1] = *greninja;
+        Combatiente.push_back(greninja);
+        Combatiente[1]->mostrarContricante();
         break;
     case 2:
-        combatiente[1] = *bulbasaur;
+        Combatiente.push_back(bulbasaur);
+        Combatiente[1]->mostrarContricante();
         break;
     case 3:
-        combatiente[1] = *charizard;
+        Combatiente.push_back(charizard);
+        Combatiente[1]->mostrarContricante();
         break;
     case 4:
-        combatiente[1] = *pikachu;
+        Combatiente.push_back(pikachu);
+        Combatiente[1]->mostrarContricante();
         break;
     default:
-        combatiente[1] = *pikachu;
+        Combatiente.push_back(pikachu);
+        Combatiente[1]->mostrarContricante();
         break;
     }
-    combatiente[0].mostrar();
-    combatiente[1].mostrar();
-    combatiente[1].atacar();
+    sanar(Combatiente[0]);
     do {
-        std::cout << "-----------------------------\n";
-        std::cout << "|    Escoge a una accion    |\n";
-        std::cout << "|       1.- Atacar          |\n";
-        std::cout << "|       2.- Curar           |\n";
-        std::cout << "-----------------------------\n";
+        std::cout << "-------------------------------------\n";
+        std::cout << "|          Escoge a una accion       |\n";
+        std::cout << "|       1.- Ataque normal (100%)     |\n";
+        std::cout << "|       1.- Ataque fuerte (50%)      |\n";
+        std::cout << "|       2.- Curar                    |\n";
+        std::cout << "--------------------------------------\n";
+        cin >> numeroeleccion;
         switch (numeroeleccion) {
-            
+        case 1:
+            Ataquenormal(Combatiente[0], Combatiente[1]);
+            break;
+        case 2:
+            acertar = rand() % 101;
+            AtaqueFuerte(Combatiente[0], Combatiente[1], acertar);
+            break;
+        case 3: 
+            sanar(Combatiente[0]);
+            break;
+        default:
+            system("CLS");
+            cout << "escoge una opcion valida\n";
+            victoria = 0;
         }
     } while (victoria != 1);
+    delete greninja;
+    delete charizard;
+    delete bulbasaur;
+    delete pikachu;
 }
 
